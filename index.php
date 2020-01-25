575d51d5408fab41d5b27451de086493a39f4ff0
Anand <anandbaburajan@gmail.com>
2020-01-25 23:39:29 +0530

    Initial setup and routes

--- index.php
+++ index.php
@@ -1,3 +1,18 @@
 <?php
-echo $_GET['url'];
- ?>
+
+$request = $_SERVER['REQUEST_URI'];
+
+switch ($request) {
+    case '/' :
+        require __DIR__ . '/views/index.php';
+        break;
+    case '' :
+        require __DIR__ . '/views/index.php';
+        break;
+    default:
+        http_response_code(404);
+        require __DIR__ . '/views/404.php';
+        break;
+}
+
+?>
