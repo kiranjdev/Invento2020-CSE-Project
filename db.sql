587b75c59db0b986dee9053189cff44a8e13ae8a
Anand <anandbaburajan@gmail.com>
2020-02-05 20:02:33 +0530

    example student home

--- db.sql
+++ db.sql
@@ -3,3 +3,6 @@ CREATE TABLE `project`.`students` ( `id` VARCHAR(10) NOT NULL , `name` VARCHAR(4
 
 /* Create Labs table */
 CREATE TABLE `project`.`labs` ( `id` INT NOT NULL AUTO_INCREMENT , `name` VARCHAR(50) NOT NULL , `code` VARCHAR(7) NOT NULL , `sem` INT NOT NULL , `year` INT NOT NULL , `active` INT NOT NULL , `teacher_1` INT NOT NULL , `teacher_2` INT NOT NULL , PRIMARY KEY (`id`)) ENGINE = InnoDB;
+
+/* Insert into Labs table example */
+INSERT INTO `labs` (`id`, `name`, `code`, `sem`, `year`, `active`, `teacher_1`, `teacher_2`) VALUES (NULL, 'Network Programming Lab', 'CS333', '6', '2020', '1', '1', '2')
--- student/index.php
+++ student/index.php
@@ -1,1 +1,24 @@
-Student dashboard
+<?php
+session_start();
+require $_SERVER['DOCUMENT_ROOT'].'/db_connect.php';
+?>
+
+<html lang="en">
+
+<head>
+  <title>Project</title>
+</head>
+
+<body>
+        <?php
+    $mysqli = OpenCon();
+     $sql = "SELECT name FROM labs WHERE sem=6 AND active=1";
+     $result = mysqli_query($mysqli, $sql);
+     while ($row = mysqli_fetch_assoc($result)) {
+         echo $row['name']."<br>";
+     }
+    CloseCon($mysqli);
+ ?>
+
+</body>
+</html>
