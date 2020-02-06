10f85ba6c52304447786579c62ffada31f6c7446
Anand <anandbaburajan@gmail.com>
2020-02-06 20:13:00 +0530

    Teachers table

--- db.sql
+++ db.sql
@@ -1,6 +1,9 @@
 /* Create Students table */
 CREATE TABLE `project`.`students` ( `id` VARCHAR(10) NOT NULL , `name` VARCHAR(42) NOT NULL , `password` VARCHAR(150) NOT NULL , `sem` INT NOT NULL , `email` VARCHAR(50) NOT NULL , `roll_no` INT NOT NULL , PRIMARY KEY (`id`)) ENGINE = InnoDB;
 
+/* Create Teachers table */
+CREATE TABLE `project`.`teachers` ( `id` INT NOT NULL AUTO_INCREMENT , `name` VARCHAR(42) NOT NULL , `password` VARCHAR(150) NOT NULL , `verified` INT NOT NULL , `email` VARCHAR(50) NOT NULL , PRIMARY KEY (`id`)) ENGINE = InnoDB;
+
 /* Create Labs table */
 CREATE TABLE `project`.`labs` ( `id` INT NOT NULL AUTO_INCREMENT , `name` VARCHAR(50) NOT NULL , `code` VARCHAR(7) NOT NULL , `sem` INT NOT NULL , `year` INT NOT NULL , `active` INT NOT NULL , `teacher_1` INT NOT NULL , `teacher_2` INT NOT NULL , PRIMARY KEY (`id`)) ENGINE = InnoDB;
 
