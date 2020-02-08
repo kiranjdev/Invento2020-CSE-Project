3d02e8682829968d465477760a9cd5336d2be2e0
Anand <anandbaburajan@gmail.com>
2020-02-08 14:31:07 +0530

    Styles and logout page

--- teacher/index.php
+++ teacher/index.php
@@ -9,8 +9,8 @@ require $_SERVER['DOCUMENT_ROOT'].'/db_connect.php';
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-    <link rel="stylesheet" href="/student/assets/css/style.css">
-    <title>Dashboard | Students</title>
+    <link rel="stylesheet" href="/teacher/assets/css/style.css">
+    <title>Dashboard | Teacher</title>
   </head>
   <body>
     <div class="container py-4">
