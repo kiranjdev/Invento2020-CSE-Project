3f6f850b3cf48e83b1664276fd438dfa503f03d4
Anand <anandbaburajan@gmail.com>
2020-02-10 00:27:41 +0530

    Added menu and footer

--- student/lab.php
+++ student/lab.php
@@ -14,14 +14,27 @@ require $_SERVER['DOCUMENT_ROOT'].'/db_connect.php';
     <title>Dashboard | Students</title>
   </head>
   <body>
-    <div class="headernav">
-      <div class="container">
-        <div class="row">
-          <div class="col-3 title"><h5>Lab</h5></div>
-          <div class="col-9 hello">Welcome, $user</div>
+    <nav class="navbar navbar-dark navbar-expand-lg customnav">
+    <div class="container">
+      <a class="navbar-brand" href="">
+        Hehe
+      </a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarResponsive">
+        <div class="navbar-nav ml-auto desktop_menu">
+          <a class="nav-item nav-link" href=""><b>Profile</b></a>
+        </div>
+        <div class="navbar-nav ml-auto phone_menu">
+          <a class="nav-item nav-link" href=""><b>Dashboard</b></a>
+          <a class="nav-item nav-link" href=""><b>Profile</b></a>
+          <a class="nav-item nav-link" href="/logout.php"><b>Logout</b></a>
         </div>
       </div>
     </div>
+  </nav>
+
     <div class="container py-4">
       <div class="row py-4">
         <div class="col-3 sidenav pr-5">
@@ -55,6 +68,12 @@ require $_SERVER['DOCUMENT_ROOT'].'/db_connect.php';
         </div>
       </div>
     </div>
+
+    <footer class="myfooter">
+      <div class="container">
+         <span class="text-muted">:D</span>
+      </div>
+    </footer>
 
   </body>
 </html>
