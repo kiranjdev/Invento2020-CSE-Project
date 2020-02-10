ef44b1dcf8d8f0632bb009e6ebfe2923ebe69335
Anand <anandbaburajan@gmail.com>
2020-02-10 11:51:18 +0530

    Login page

--- index.php
+++ index.php
@@ -1,1 +1,41 @@
-Common Login
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link href="https://fonts.googleapis.com/css?family=Poppins&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link rel="stylesheet" href="/assets/css/style.css">
+    <title>Login</title>
+  </head>
+  <body>
+  <!--- <div class="alert alert-warning logauth">The password you entered was not valid. / No account found with that username.</div> --->
+	<div class="limiter">
+		<div class="container-login100">
+			<div class="wrap-login100">
+				<form class="login100-form validate-form">
+					<span class="login100-form-title p-b-26">
+						Welcome
+					</span><br><br>
+
+					<div class="wrap-input100 validate-input">
+						<input class="input100" type="text" id="username" name="username" placeholder="Username" required>
+					</div>
+
+					<div class="wrap-input100 validate-input">
+						<input class="input100" type="password" id="password" name="pass" placeholder="Password" required>
+					</div>
+
+					<button class="login100-form-btn" id="login" name="login" type="submit">Login</button>
+
+					<div class="text-center newreg">
+						<a href="">New Student?</a>
+            <a href="">New Teacher?</a>
+					</div>
+				</form>
+			</div>
+		</div>
+	</div>
+
+</body>
+</html>
